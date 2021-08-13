b3b9d3a23f939ce42f09f5e54751f4b4ad99b695
陈惠东 <chderonwill@gmail.com>
2021-08-13 15:43:46 +0800

    Update BahamutAdult.js
    动画疯年龄限制远程订阅

--- NobyDa/Surge/JS/BahamutAdult.js
+++ NobyDa/Surge/JS/BahamutAdult.js
@@ -7,8 +7,8 @@ http-request https:\/\/api\.gamer\.com\.tw\/mobile_app\/anime\/v3\/token\.php sc
 http-response https:\/\/api\.gamer\.com\.tw\/mobile_app\/anime\/v3\/token\.php requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Bahamut.js
 
 QX 1.0.5 : [rewrite_local] 
-https:\/\/api\.gamer\.com\.tw\/mobile_app\/anime\/v1\/vip_info\.php url script-request-header BahamutAdult.js
-https:\/\/api\.gamer\.com\.tw\/mobile_app\/anime\/v1\/vip_info\.php url script-response-body BahamutAdult.js
+https:\/\/api\.gamer\.com\.tw\/mobile_app\/anime\/v1\/vip_info\.php url script-request-header https://raw.githubusercontent.com/UncleDeron/QuantumultX/master/NobyDa/Surge/JS/BahamutAdult.js
+https:\/\/api\.gamer\.com\.tw\/mobile_app\/anime\/v1\/vip_info\.php url script-response-body https://raw.githubusercontent.com/UncleDeron/QuantumultX/master/NobyDa/Surge/JS/BahamutAdult.js
 
 
 QX or Surge MITM = api.gamer.com.tw
